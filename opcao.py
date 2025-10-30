3984dfe49452d961afb0bf736ef374d5c84f678a
franciscomororo1 <junior.mororo@gmail.com>
2025-10-30 22:27:33 +0000

    Atualizando classe Opção para POO

--- opcao.py
+++ opcao.py
@@ -1,15 +1,17 @@
 from biblioteca_b4 import biblioteca
 import sys
 
-def escolha():
-    while True:
-        continuar = str(input("Deseja repetir a operação? (s/n)"))
-        match continuar:
-            case "s" | "S":
-                print("Você escolheu continuar:")
-                return True
-            case "n" | "N":
-                print("Você escolheu finalizar.")
-                sys.exit()
-            case _:
-                print("Opção inválida, digite apenas 's' ou 'n'.")
+class Opcao:
+
+   def escolha():
+       while True:
+           continuar = str(input("Deseja repetir a operação? (s/n)"))
+           match continuar:
+               case "s" | "S":
+                   print("Você escolheu continuar:")
+                   return True
+               case "n" | "N":
+                   print("Você escolheu finalizar.")
+                   sys.exit()
+               case _:
+                   print("Opção inválida, digite apenas 's' ou 'n'.")
